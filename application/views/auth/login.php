6e7de6c853bba2c30bfdb587557297e66278162e
ArkanIsa <arkandoid@gmail.com>
2026-04-08 11:59:46 +0700

    Digital Pembiayaan UMKM

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -1,28 +1,34 @@
-<h1><?php echo lang('login_heading');?></h1>
-<p><?php echo lang('login_subheading');?></p>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$this->load->view('layouts/header');
+?>
 
-<div id="infoMessage"><?php echo $message;?></div>
+<div class="row h-100">
+  <div class="col-lg-5 col-12">
+    <div id="auth-left">
+      <h1 class="auth-title"><?php echo lang('login_heading'); ?></h1>
+      <br>
 
-<?php echo form_open("auth/login");?>
+      <?php echo form_open("auth/login"); ?>
+      <div class="form-group position-relative has-icon-left mb-4">
+        <?php echo form_input($identity + ['class' => 'form-control form-control-xl', 'placeholder' => lang('login_identity_label')]); ?>
+        <div class="form-control-icon">
+          <i class="bi bi-person"></i>
+        </div>
+      </div>
+      <div class="form-group position-relative has-icon-left mb-4">
+        <?php echo form_input($password + ['class' => 'form-control form-control-xl', 'placeholder' => lang('login_password_label')]); ?>
+        <div class="form-control-icon">
+          <i class="bi bi-shield-lock"></i>
+        </div>
+      </div>
+      <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5"
+        type="submit"><?php echo lang('login_submit_btn'); ?></button>
+      <?php echo form_close(); ?>
 
-  <p>
-    <?php echo lang('login_identity_label', 'identity');?>
-    <?php echo form_input($identity);?>
-  </p>
 
-  <p>
-    <?php echo lang('login_password_label', 'password');?>
-    <?php echo form_input($password);?>
-  </p>
-
-  <p>
-    <?php echo lang('login_remember_label', 'remember');?>
-    <?php echo form_checkbox('remember', '1', FALSE, 'id="remember"');?>
-  </p>
-
-
-  <p><?php echo form_submit('submit', lang('login_submit_btn'));?></p>
-
-<?php echo form_close();?>
-
-<p><a href="forgot_password"><?php echo lang('login_forgot_password');?></a></p>
+      <div class="col-lg-7 d-none d-lg-block">
+        <div id="auth-right">
+        </div>
+      </div>
+    </div>
